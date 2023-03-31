26798b5b5d65d34efb389437a51cc3376fad486b
Seba <sebol8140@gmail.com>
2023-03-31 10:48:22 +0200

    metoda 1 i 2

--- gaderypoluki/Program.cs
+++ gaderypoluki/Program.cs
@@ -35,9 +35,66 @@ string gaderypuluki(string clearText)
     return cypherText;
 }
 
+string gaderypoluki2(string clearText)
+{
+    string cypherText = "";
+
+    foreach(char c in clearText)
+    {
+        //wykonaj poniższe imstrukcje dla każdej litery w tekście źródłowym
+        // c = jedna literka z tekstu
+        switch (c)
+        {
+            case 'G':
+                cypherText += 'A';
+                break;
+            case 'A':
+                cypherText+= 'G';
+                break;
+            case 'D':
+                cypherText += 'E';
+                break;
+            case 'E':
+                cypherText += "D";
+                break;
+            case 'R':
+                cypherText += 'Y';
+                break;
+            case 'Y':
+                cypherText += "R";
+                break;
+            case 'P':
+                cypherText += "O";
+                break;
+            case 'O':
+                cypherText += "P";
+                break;
+            case 'L':
+                cypherText += 'U';
+                break;
+            case 'U':
+                cypherText += 'L';
+                break;
+            case 'K':
+                cypherText += 'I';
+                break;
+            case 'I':
+                cypherText += 'K';
+                break;
+            default:
+                //jeżeli nie pasuje do wzorca to przepisz istniejąca litere
+                cypherText+= c;
+                break;  
+        }
+    }
+    return cypherText;
+}
+
 //tekst do zaszyfrowania
 string clearText = "GADERYPOLUKI";
 //zaszyfruj używając funkcji
 string cypherText = gaderypuluki(clearText);
 
-Console.WriteLine("Zaszyfrowanyy tekst: " + cypherText);
+Console.WriteLine("Zaszyfrowanyy tekst (metoda1): " + cypherText);
+
+Console.WriteLine("Zaszyfrowany tekst (metoda2): " + gaderypoluki2(clearText));
